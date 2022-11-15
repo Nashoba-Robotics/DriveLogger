cb73e5c90c744e280328f03ef647154892f80075
0418yicheng <0418yicheng@gmail.com>
2022-11-15 18:44:24 -0500

    Implemented logging for step function

--- src/main/java/frc/robot/commands/StepCommand.java
+++ src/main/java/frc/robot/commands/StepCommand.java
@@ -8,20 +8,52 @@ import frc.robot.subsystems.DriveSubsystem;
 
 public class StepCommand extends CommandBase {
     DataLog log;
-    DoubleLogEntry entry;
+    DoubleLogEntry leftMotorVelocity;
+    DoubleLogEntry leftStatorCurrent;
+    DoubleLogEntry leftSupplyCurrent;
+
+    DoubleLogEntry rightMotorVelocity;
+    DoubleLogEntry rightStatorCurrent;
+    DoubleLogEntry rightSupplyCurrent;
+    
+    DoubleLogEntry pigeonAccelerationX;
+    DoubleLogEntry pigeonAccelerationY;
+    DoubleLogEntry pigeonAccelerationZ;
 
     @Override
     public void initialize() {
         DataLogManager.start();
         log = DataLogManager.getLog();
-        entry = new DoubleLogEntry(log, "LeftMotorVelocity");
 
-        DriveSubsystem.getInstance().set(1.);
+        leftMotorVelocity = new DoubleLogEntry(log, "LeftMotorVelocity");
+        leftStatorCurrent = new DoubleLogEntry(log, "LeftStatorCurrnet");
+        leftSupplyCurrent = new DoubleLogEntry(log, "LeftSupplyCurrent");
+
+        rightMotorVelocity = new DoubleLogEntry(log, "RightMotorVelocity");
+        rightStatorCurrent = new DoubleLogEntry(log, "RightStatorCurrnet");
+        rightSupplyCurrent = new DoubleLogEntry(log, "RightSupplyCurrent");
+
+        pigeonAccelerationX = new DoubleLogEntry(log, "PigeonAccelerationX");
+        pigeonAccelerationY = new DoubleLogEntry(log, "PigeonAccelerationY");
+        pigeonAccelerationZ = new DoubleLogEntry(log, "PigeonAccelerationZ");
+
+
+        DriveSubsystem.getInstance().set(1);
     }
 
     @Override
     public void execute() {
-        entry.append(DriveSubsystem.getInstance().getLeftMotorVelocity());
+        leftMotorVelocity.append(DriveSubsystem.getInstance().getLeftMotorVelocity());
+        leftStatorCurrent.append(DriveSubsystem.getInstance().getLeftStatorCurrent());
+        leftSupplyCurrent.append(DriveSubsystem.getInstance().getLeftSupplyCurrent());
+
+        rightMotorVelocity.append(DriveSubsystem.getInstance().getRightMotorVelocity());
+        rightStatorCurrent.append(DriveSubsystem.getInstance().getRightStatorCurrent());
+        rightSupplyCurrent.append(DriveSubsystem.getInstance().getRightSupplyCurrent());
+
+        pigeonAccelerationX.append(DriveSubsystem.getInstance().getAccelX());
+        pigeonAccelerationY.append(DriveSubsystem.getInstance().getAccelY());
+        pigeonAccelerationZ.append(DriveSubsystem.getInstance().getAccelZ());
     }
 
     @Override
